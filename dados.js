9fa043202f80c711875fdc82d0970cbd028ccb5b
Luiz Fernando <macedoovale@gmail.com>
2024-09-07 21:09:46 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,104 @@
+const bandas = [
+    {
+        nome: "Metallica",
+        descricao: "Uma das bandas de thrash metal mais influentes do mundo, formada em 1981. Conhecida por álbuns clássicos como Master of Puppets e Ride the Lightning.",
+        imagem: "assets/metallica.jpg",
+        link: "https://pt.wikipedia.org/wiki/Metallica"
+    },
+    {
+        nome: "Iron Maiden",
+        descricao: "Banda britânica de heavy metal, formada em 1975. Reconhecida por seus mascotes Eddie e canções como Run to the Hills e The Number of the Beast.",
+        imagem: "assets/iron-maiden.jpg",
+        link: "https://pt.wikipedia.org/wiki/Iron_Maiden"
+    },
+    {
+        nome: "Guns N' Roses",
+        descricao: "Guns N' Roses (por vezes abreviado como G N' R ou GnR) é uma banda norte-americana de hard rock formada em Los Angeles, Califórnia, em 1985.",
+        imagem: "assets/guns-n-roses.jpg",
+        link: "https://pt.wikipedia.org/wiki/Guns_N%27_Roses"
+    },
+    {
+        nome: "Led Zeppelin",
+        descricao: "Uma das bandas mais influentes do rock, formada em 1968, com sucessos como Stairway to Heaven e Whole Lotta Love.",
+        imagem: "assets/led-zeppelin.jpg",
+        link: "https://pt.wikipedia.org/wiki/Led_Zeppelin"
+    },
+    {
+        nome: "Pink Floyd",
+        descricao: "Banda britânica de rock progressivo conhecida pelos álbuns The Dark Side of the Moon e The Wall. Formada em 1965.",
+        imagem: "assets/pink-floyd.jpg",
+        link: "https://pt.wikipedia.org/wiki/Pink_Floyd"
+    },
+    {
+        nome: "Queen",
+        descricao: "Banda britânica formada em 1970, conhecida por sucessos como Bohemian Rhapsody e We Will Rock You.",
+        imagem: "assets/queen.jpg",
+        link: "https://pt.wikipedia.org/wiki/Queen_(banda)"
+    },
+    {
+        nome: "AC/DC",
+        descricao: "Banda australiana de hard rock formada em 1973, famosa por hits como Highway to Hell e Back in Black.",
+        imagem: "assets/acdc.jpg",
+        link: "https://pt.wikipedia.org/wiki/AC/DC"
+    },
+    {
+        nome: "Nirvana",
+        descricao: "Banda grunge dos EUA formada em 1987, liderada por Kurt Cobain, conhecida por álbuns como Nevermind e In Utero.",
+        imagem: "assets/nirvana.jpg",
+        link: "https://pt.wikipedia.org/wiki/Nirvana_(banda)"
+    },
+    {
+        nome: "The Rolling Stones",
+        descricao: "Uma das bandas de rock mais icônicas, formada em 1962, conhecida por músicas como Paint It Black e Sympathy for the Devil.",
+        imagem: "assets/rolling-stones.jpg",
+        link: "https://pt.wikipedia.org/wiki/The_Rolling_Stones"
+    },
+    {
+        nome: "Foo Fighters",
+        descricao: "Banda de rock formada em 1994 por Dave Grohl após o fim do Nirvana, conhecida por álbuns como The Colour and the Shape e Wasting Light.",
+        imagem: "assets/foo-fighters.jpeg",
+        link: "https://pt.wikipedia.org/wiki/Foo_Fighters"
+    },
+    {
+        nome: "Red Hot Chili Peppers",
+        descricao: "Banda americana de rock alternativo formada em 1983, famosa por hits como Under the Bridge e Californication.",
+        imagem: "assets/red-hot-chili-peppers.jpg",
+        link: "https://pt.wikipedia.org/wiki/Red_Hot_Chili_Peppers"
+    },
+    {
+        nome: "Black Sabbath",
+        descricao: "Pioneiros do heavy metal, formados em 1968, conhecidos por álbuns como Paranoid e Sabbath Bloody Sabbath.",
+        imagem: "assets/black-sabbath.jpg",
+        link: "https://pt.wikipedia.org/wiki/Black_Sabbath"
+    },
+    {
+        nome: "Deep Purple",
+        descricao: "Banda britânica de hard rock formada em 1968, famosa por músicas como Smoke on the Water e Highway Star.",
+        imagem: "assets/deep-purple.jpg",
+        link: "https://pt.wikipedia.org/wiki/Deep_Purple"
+    },
+    {
+        nome: "Angra",
+        descricao: "Banda britânica de rock formada em 1964, conhecida por álbuns como Tommy e Who's Next.",
+        imagem: "assets/angra.jpg",
+        link: "https://pt.wikipedia.org/wiki/Angra_(banda)"
+    },
+    {
+        nome: "Soundgarden",
+        descricao: "Banda grunge dos EUA formada em 1984, conhecida por álbuns como Superunknown e Badmotorfinger.",
+        imagem: "assets/soundgarden.jpg",
+        link: "https://pt.wikipedia.org/wiki/Soundgarden"
+    },
+    {
+        nome: "Stone Temple Pilots",
+        descricao: "Banda de rock alternativo formada em 1989, conhecida por hits como Plush e Interstate Love Song.",
+        imagem: "assets/stone-temple-pilots.jpg",
+        link: "https://pt.wikipedia.org/wiki/Stone_Temple_Pilots"
+    },
+    {
+        nome: "Oasis",
+        descricao: "Banda britânica de rock formada em 1991, conhecida por sucessos como Wonderwall e Don't Look Back in Anger.",
+        imagem: "assets/oasis.jpg",
+        link: "https://pt.wikipedia.org/wiki/Oasis_(banda)"
+    }
+];
